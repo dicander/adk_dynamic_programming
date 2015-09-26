e5e76b2f1ec07b224621efa3e6074353ce9a2327
Marcus Dicander <dicander@gmail.com>
2015-09-26 03:48:36 +0200

    improved printouts

--- protein_folding.cpp
+++ protein_folding.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+static const int dbg = 1;
+
 
 int profit(const string& chain, int a, int b, int c) {
 	int shortest=min(b-a,c-(b+1));
 	int s=0;
 	for(int i=1; i<=shortest; ++i) {
 		if (chain[b-i]=='1' && chain[b+1+i]=='1') {
-			s=s+1;
+			++s;
 		}
 	}
 	return s;
 }
 
-void print_matrix(const string& chain, const vector<vector<int>>& q) {
-	int n = q.size();
+void print_matrix(const string& chain, const vector<vector<int>>& q, int n) {
 	printf("idx");
 	for(int i=1; i<n; ++i) {
 		printf("%3d ", i);
@@ -23,6 +24,10 @@ void print_matrix(const string& chain, const vector<vector<int>>& q) {
 	printf("str");
 	for(int i=1; i<n; ++i) {
 		printf("  %c ", chain[i]);
+	}
+	printf("\n===");
+	for(int i=1; i<n; ++i) {
+		printf("====");
 	}
 	printf("\n");
 	for(int i=1; i<n; ++i) {
@@ -36,7 +41,7 @@ void print_matrix(const string& chain, const vector<vector<int>>& q) {
 
 int dynprog(const string& chain) {
 	int n = chain.size();
-	vector<vector<int>> q(n,vector<int>(n,0));
+	vector<vector<int>> q(n+2,vector<int>(n+2,0));
 
 	for(int b=n-1; b >= 2; --b) {
 
@@ -48,7 +53,7 @@ int dynprog(const string& chain) {
 			q[a][b]=t;
 		}
 	}
-	print_matrix(chain, q);
+	print_matrix(chain, q, n);
 	int max_score=0;
 	for(int b = 2; b<=n; ++b) {
 		if(q[1][b]>max_score) {max_score=q[1][b]; }
